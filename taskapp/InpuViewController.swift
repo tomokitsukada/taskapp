1c1c9125a103293e96e9a62eff2d56bd177f9e78
tomoki.tsukada <chyacoco@gmail.com>
2020-09-03 21:16:46 +0900

    pod installと.gitignoreの作成まで実施

---
+++ taskapp/InpuViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InpuViewController.swift
+//  taskapp
+//
+//  Created by 塚田朋来 on 2020/09/03.
+//  Copyright © 2020 tomoki.tsukada. All rights reserved.
+//
+
+import UIKit
+
+class InpuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
